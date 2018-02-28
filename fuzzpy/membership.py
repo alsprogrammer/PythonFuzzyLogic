e310c5d89c49319090bdb592d7a0bd696343dcdb
Александр <alstutor@gmail.com>
2018-02-28 10:12:14 +0700

    TriFunc is fixed, now it produces correct values

--- fuzzpy/membership.py
+++ fuzzpy/membership.py
@@ -27,4 +27,5 @@ class TriFunc(MembershipFunction):
     The triangle membership function.
     """
     def __init__(self, coords=[-1, 0, 1]):
-        self.func = lambda x: 0 if x <= coords[0] else x / (coords[1] - coords[0]) + 1 if x <= coords[1] else x * (-1 / (coords[2] - coords[1])) + 1 if x <= coords[2] else 0
+        #self.func = lambda x: 0 if x <= coords[0] else x / (coords[1] - coords[0]) + 1 if x <= coords[1] else x * (-1 / (coords[2] - coords[1])) + 1 if x <= coords[2] else 0
+        self.func = lambda x: 0 if x <= coords[0] or x >= coords[2] else (x - coords[0]) / (coords[1] - coords[0]) if x <= coords[1] else 1 - (x - coords[1]) / (coords[2] - coords[1])
