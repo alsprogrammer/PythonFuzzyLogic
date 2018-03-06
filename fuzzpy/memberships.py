18a1ed006d5dd71449593dafb206ca99877b9374
alsprogrammer <alstutor@gmail.com>
2018-03-06 17:15:08 +0700

    The test set was extended.

--- fuzzpy/memberships.py
+++ fuzzpy/memberships.py
@@ -83,24 +83,3 @@ class TrapecFunc(MembershipFunction):
             return ret
 
         self.func = trapfunc
-
-
-if __name__ == "__main__":
-    left1 = -10
-    center1 = 0
-    right1 = 10
-    left2 = 0
-    center2 = 10
-    right2 = 20
-    step = 5
-
-    func1 = TriFunc(left1 / 10.0, center1 / 10.0, right1 / 10.0)
-    func2 = TriFunc(left2 / 10.0, center2 / 10.0, right2 / 10.0)
-
-    func = func1 & func2
-    for x in range(left1 - step, right2 + step, step):
-        x1 = x / 10.0
-        f1 = func1(x1)
-        f2 = func2(x1)
-        f = func(x1)
-        print("x={x}, f1={f1}, f2={f2}, composition={c}".format(x=x, f1=f1, f2=f2, c=f))
---
+++ tests/variables_test.py
@@ -0,0 +1,4 @@
+import unittest
+from fuzzpy.variables import FuzzyVariable, FuzzyTerm, FuzzyRule
+
+
