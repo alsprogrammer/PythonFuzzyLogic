f99cf288674bdc41cf3bb542fe64ef905143ae8a
Александр <alstutor@gmail.com>
2018-03-12 22:02:12 +0700

    The test coverage was improved.

--- tests/membership_functions_test.py
+++ tests/membership_functions_test.py
@@ -30,7 +30,7 @@ class MembershipFunctionsTestCase(unittest.TestCase):
 
         for left, left1, right1, right in limits:
             func = TrapecFunc(left / 10.0, left1 / 10.0, right1 / 10.0, right / 10.0)
-            for x in range(left - step, right + step, step):
+            for x in range(left - step, right + 2 * step, step):
                 x1 = x / 10.0
                 f = func(x1)
                 if x <= left:
@@ -50,7 +50,7 @@ class MembershipFunctionsTestCase(unittest.TestCase):
         right = 0
         step = 5
         func = TriFunc(left / 10.0, center / 10.0, right / 10.0)
-        for x in range(left - step, right + step, step):
+        for x in range(left - step, right + 2 * step, step):
             x1 = x / 10.0
             if x <= left:
                 self.assertEqual(func(x1), 0)
@@ -71,10 +71,6 @@ class MembershipFunctionsTestCase(unittest.TestCase):
             x1 = x / 10.0
             if x <= left:
                 self.assertEqual(func(x1), 0)
-            elif x < center:
-                self.assertEqual(func(x1), 0.5)
-            elif x == center:
-                self.assertEqual(func(x1), 1)
             elif x < right:
                 self.assertEqual(func(x1), 0.5)
             else:
@@ -125,9 +121,6 @@ class AndOrMulMembershipFunctionsTestCase(unittest.TestCase):
     """
     The test of the application of "and" and "or" operators.
     """
-    def SetUp(self):
-        pass
-
     def test_and_not_intersected_trifunc(self):
         left1 = -10
         center1 = 0
@@ -188,26 +181,6 @@ class AndOrMulMembershipFunctionsTestCase(unittest.TestCase):
             f = func(x1)
             self.assertEqual(f, max(f1, f2))
 
-    def test_and_not_intersected_trifunc(self):
-        left1 = -10
-        center1 = 0
-        right1 = 10
-        left2 = 10
-        center2 = 20
-        right2 = 30
-        step = 5
-
-        func1 = TriFunc(left1 / 10.0, center1 / 10.0, right1 / 10.0)
-        func2 = TriFunc(left2 / 10.0, center2 / 10.0, right2 / 10.0)
-
-        func = func1 & func2
-        for x in range(left1 - step, right2 + step, step):
-            x1 = x / 10.0
-            f1 = func1(x1)
-            f2 = func2(x1)
-            f = func(x1)
-            self.assertEqual(f, 0)
-
     def test_or_not_intersected_trifunc(self):
         left1 = -10
         center1 = 0
@@ -247,27 +220,3 @@ class AndOrMulMembershipFunctionsTestCase(unittest.TestCase):
             f2 = func2(x1)
             f = func(x1)
             self.assertEqual(f, f1 * f2)
-
-    def test_mul_not_intersected_trifunc(self):
-        left1 = -10
-        center1 = 0
-        right1 = 10
-        left2 = 10
-        center2 = 20
-        right2 = 30
-        step = 5
-
-        func1 = TriFunc(left1 / 10.0, center1 / 10.0, right1 / 10.0)
-        func2 = TriFunc(left2 / 10.0, center2 / 10.0, right2 / 10.0)
-
-        func = func1 * func2
-        for x in range(left1 - step, right2 + step, step):
-            x1 = x / 10.0
-            f1 = func1(x1)
-            f2 = func2(x1)
-            f = func(x1)
-            self.assertEqual(f, 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
