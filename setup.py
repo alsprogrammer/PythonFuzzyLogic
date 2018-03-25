ed605cae7b2b21940679947cd523c4dcf807fef3
Александр <alstutor@gmail.com>
2018-03-25 20:50:36 +0700

    The link to the github repository was added to the setup.py file.

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
     version=fuzzypy.__version__,
     author='Aleksandr Saiapin',
     author_email='alstutor@gmail.com',
+    url='https://github.com/alsprogrammer/PythonFuzzyLogic',
     packages=find_packages(),
     test_suite='tests',
     long_description="The library for working with fuzzy logic in python. This library is developed to be used for "
                      "modelling the fuzzy sets and fuzzy controllers. The library cab be used both in production and "
-                     "for educational purposes.",
+                     "for educational purposes. For example please visit the github repository",
 )
